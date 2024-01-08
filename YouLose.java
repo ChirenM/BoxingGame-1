622e1266303330d9528593c693bfe063d538c953
ChirenM <152298288+ChirenM@users.noreply.github.com>
2024-01-08 00:59:46 +0000

    Update YouLose.java

--- YouLose.java
+++ YouLose.java
@@ -1,9 +1,9 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class YouLose here.
+ * You lose screen
  * 
- * @author (your name) 
+ * @author (Elizabeth)
  * @version (a version number or a date)
  */
 public class YouLose extends Actor
