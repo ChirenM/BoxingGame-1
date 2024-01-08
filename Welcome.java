3a200807d4bddcd93e2ae5b31e2c28e7291881b6
ChirenM <152298288+ChirenM@users.noreply.github.com>
2024-01-08 00:59:22 +0000

    Update Welcome.java

--- Welcome.java
+++ Welcome.java
@@ -1,9 +1,9 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class Welcome here.
+ * Welcome screen
  * 
- * @author (your name) 
+ * @author (Elizabeth)
  * @version (a version number or a date)
  */
 public class Welcome extends Actor
